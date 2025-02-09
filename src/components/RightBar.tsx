d1c221ac5c0320092400b8431820469afb12d932
obszank <obszank@gmail.com>
2025-02-09 09:03:11 +0100

    Add Search, Recommendations, PopularTags components and integrate them into RightBar

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -1,6 +1,23 @@
+import Link from "next/link"
+import PopularTags from "./PopularTags"
+import Recommendations from "./Recommendations"
+import Search from "./Search"
+
 const RightBar = () => {
   return (
-    <div className=''>RightBar</div>
+    <div className='pt-4 flex flex-col gap-4 sticky top-0 h-max'>
+      <Search />
+      <PopularTags />
+      <Recommendations />
+      <div className="text-textGray text-sm flex gap-x-4 flex-wrap">
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+        <Link href="/">Cookie Policy</Link>
+        <Link href="/">Accessibility</Link>
+        <Link href="/">Ads Info</Link>
+        <span>© 2025 obszank</span>
+      </div>
+    </div>
   )
 }
 
